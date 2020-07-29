cb25b26a4266fbbc2cbdb0b96c0e5d73b78ca957
NikithKS <nikithks007@gmail.com>
2020-07-29 22:36:15 +0530

    Flask Practice

---
+++ todo/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCEHMY_DATABASE_URI'] = 'sqlite:///items.db'
+
+db = SQLAlchemy(app)
+
+class items(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    task = db.Column(db.Text, nullable = True)
+    date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+
+    def __repr__(self):
+        return 'Task' + str(self.id)
+
+
