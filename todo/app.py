69d44bbaa9d6624964bbda960117f950bba2f4ee
NikithKS <nikithks007@gmail.com>
2020-07-30 13:24:39 +0530

    Project To-do

--- todo/app.py
+++ todo/app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCEHMY_DATABASE_URI'] = 'sqlite:///items.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todoItems.db'
 
 db = SQLAlchemy(app)
 
@@ -11,14 +12,35 @@ class items(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     task = db.Column(db.Text, nullable = True)
-    date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    date = db.Column(db.DateTime, nullable = False, default = datetime.now())
+    status = db.Column(db.Integer, default = 0)
 
 
     def __repr__(self):
         return 'Task' + str(self.id)
 
 
-@all.route("/")
+@app.route("/")
 def home():
-    
+    content = items.query.all()
+    return render_template('main.html', data = content[::-1])
 
+@app.route("/new", methods = ['POST'])
+def addnew():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('new.html')
+
+
+@app.route("/edit/<int:current_id>", methods = ["POST"])
+def edit(current_id):
+    if request.method == 'POST':
+        pass
+    else:
+        info = items.query.all()[current_id]
+        return render_template('editor.html', item = info)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
