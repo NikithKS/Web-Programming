a95a6cadbf6c8c661c3e37f52a55fb81c1cb43a8
NikithKS <nikithks007@gmail.com>
2020-08-04 13:11:15 +0530

    JS Stuff

--- Basic JS/everything.js
+++ Basic JS/everything.js
@@ -1,12 +1,34 @@
+function ready(){
+    document.getElementById("thats-onload").innerHTML = "Image Load...";
+    document.getElementById("loading-image").onload = loaded();
+}
+
+function loaded(){
+    document.getElementById("thats-onload").innerHTML = "Image Loaded.";
+
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
 function change(){
-    const content = document.getElementById('content');
-    const button = document.getElementById('butt')
+    const content = document.getElementById('hello-content');
     if(content.innerHTML === "Hello World!"){
         content.innerHTML = "Namaste World!";
-        button.innerHTML = "Go Western";
+        document.getElementById('hello-button').innerHTML = "Go Western";
     }
     else {
         content.innerHTML = "Hello World!";
-        button.innerHTML = "Go Desi";
+        document.getElementById('hello-button').innerHTML = "Go Desi";
     }
+    tempAlert("That was using onclick event!", 3000);
+}
+
+function tempAlert(msg, forTime) {
+    var blk = document.createElement("div")
+    blk.setAttribute("style", "position:absolute; top:5%; right:10%; background-color:lightyellow; border: solid 3px yellow; padding:10px; border-radius:5px; opacity: 0.75;");
+    blk.innerHTML = msg;
+    setTimeout (function(){
+        blk.parentNode.removeChild(blk);
+    }, forTime);
+    document.body.appendChild(blk);
 }
