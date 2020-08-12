bb70606ec6ac554c4b61c9de713f734d5dd6588f
NikithKS <nikithks007@gmail.com>
2020-08-12 09:38:22 +0530

    Node JS 1st program

---
+++ Basic JS/NodeJS/logger.js
@@ -0,0 +1,10 @@
+function Ulogger(message) {
+    console.log(message.toUpperCase());
+}
+
+function Llogger(message) {
+    console.log(message.toLowerCase());
+}
+
+
+module.exports = {Ulogger, Llogger};
---
+++ Basic JS/NodeJS/main.js
@@ -0,0 +1,7 @@
+const logger = require("./logger");
+
+// import logger from "./logger" as extLogger;
+const extLogger = require("./logger");
+
+extLogger.Llogger("Hello there");
+extLogger.Ulogger("Hello there");
